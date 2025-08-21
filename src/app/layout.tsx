c14ebca5c1952f7b1c67d121be25fcef01948d99
TakshiTech <takshitechdigital@gmail.com>
2025-08-22 00:42:09 +0530

    GTM tag added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,15 +11,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-NKB6C878');`,
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NB5LG6FF');`,
           }}
         />
         
 
-        {/* Google tag (gtag.js) */}
+        {/* Google tag (gtag.js)
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17158201553"></script>
         <script
           dangerouslySetInnerHTML={{
@@ -30,8 +30,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               gtag('config', 'AW-17158201553');
             `,
           }}
-        />
-        {/* Google tag (gtag.js) */}
+        /> */}
+
+        {/* Google tag (gtag.js)
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-R7LS5LC8F4"></script>
         <script
           dangerouslySetInnerHTML={{
@@ -42,8 +43,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               gtag('config', 'G-R7LS5LC8F4');
             `,
           }}
-        />
-         <script
+        /> */}
+
+{/* Brevo API CONVERSATIONS */}
+
+         {/* <script
           dangerouslySetInnerHTML={{
             __html: `
               (function(d, w, c) {
@@ -58,15 +62,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               })(document, window, 'BrevoConversations');
             `,
           }}
-        />
+        /> */}
+
+
       </head>
 
       <body>
         {/* Google Tag Manager (noscript) */}
         <noscript
           dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NKB6C878"
-              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NB5LG6FF"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
           }}
         />
 
