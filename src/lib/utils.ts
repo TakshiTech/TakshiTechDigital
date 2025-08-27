eee57b6a08c7ed981f65a6ec61aa5d00f9e26530
TakshiTech <takshitechdigital@gmail.com>
2025-08-28 01:02:47 +0530

    redeploy

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
-import { ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+// src/lib/utils.ts
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
